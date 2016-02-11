d02b28875f0af1eb9ddc4d083701d1de9f21c4e3
Mátyás Mustoha <mmatyas@inf.u-szeged.hu>
2016-02-11 11:57:40 +0100

    Rust stdlib script frissítése

--- build-rust-stdlibs-nightlyscript.sh
+++ build-rust-stdlibs-nightlyscript.sh
@@ -6,25 +6,51 @@ set -o xtrace
 
 date +%F\ %T
 
-cd /home/mmatyas/Mozilla/rustcrossbuild
-rm -f rust-snapshot-hash
-wget https://raw.githubusercontent.com/servo/servo/master/rust-snapshot-hash
+# Check if hash changed
+rm -f rust-snapshot_date
+wget https://raw.githubusercontent.com/servo/servo/master/rust-nightly-date -O rust-snapshot_date
+if [[ ! $(diff rust-snapshot_date rust-snapshot_date-current) ]]; then exit 1; fi
 
-diff rust-snapshot-hash rust-snapshot-hash-current
-if [[ $? -eq 0 ]]; then exit $?; fi
+time_start=$(date +%s)
 
-cd rust
+# Get Rust hash of Servo
+cd servo
+git pull
+./mach bootstrap-rust --force
+rusthash=`./mach rustc -vV | awk 'NR==3' | cut -d' ' -f2`
+
+# Checkout Rust
+cd ../rust
 git checkout master
 git pull
-
-rusthash=`git log --until="$(cat ../rust-snapshot-hash)" --oneline | head -n 1 | cut -d ' ' -f 1`
 git checkout $rusthash
+git rev-parse HEAD
 
+# Build
 mkdir -p build && cd build
-../configure --target=arm-unknown-linux-gnueabihf,aarch64-unknown-linux-gnu,x86_64-unknown-linux-gnu --prefix=$(pwd)/install_$rusthash --disable-docs --enable-ccache
+../configure \
+--target=\
+arm-unknown-linux-gnueabihf,aarch64-unknown-linux-gnu,\
+arm-linux-androideabi,aarch64-linux-android,\
+x86_64-unknown-linux-gnu \
+--arm-linux-androideabi-ndk="$ANDROID_TOOLCHAIN_ARMHF" \
+--aarch64-linux-android-ndk="$ANDROID_TOOLCHAIN_ARM64" \
+--prefix=$(pwd)/install_$rusthash --disable-docs --enable-ccache
 make -j4
 make install
-tar czf rust-armhf-aarch64-$rusthash.tgz -C install_$rusthash .
-cp rust-armhf-aarch64-$rusthash.tgz /var/www/html/servo/rust-cross/
 
-cp rust-snapshot-hash ../rust-snapshot-hash-current
+# Deploy
+webpath=/var/www/html/servo/rust-cross
+tar czf rust-armhf_aarch64_android-$rusthash.tgz -C install_$rusthash .
+cp rust-armhf_aarch64_android-$rusthash.tgz $webpath
+
+# Set as latest
+rm $webpath/rust-armhf_aarch64_android-latest.tgz
+ln -s rust-armhf_aarch64_android-$rustver.tgz $webpath/rust-armhf_aarch64_android-latest.tgz
+
+# Remember hash
+cd ../..
+cp rust-snapshot_date rust-snapshot_date-current
+
+time_end=$(date +%s)
+runtime=$((time_end-time_start))
